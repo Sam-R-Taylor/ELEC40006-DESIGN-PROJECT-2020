e75260fdf95bf60ca53994aabe5d58334e8680a0
lg519 <lg519@ic.ac.uk>
2020-05-19 16:21:25 +0100

    added Diode implementation

--- Parser.cpp
+++ Parser.cpp
@@ -35,14 +35,18 @@ int main(int argc, char const *argv[])
     else if (tmp == 'L')            //Inductor added to _circuit
     {
         src >> _name >> _anode >> _cathode >> _value;
-        _circuit.add_component(Resistor(_anode,_cathode,_name,_value));
+        _circuit.add_component(Inductor(_anode,_cathode,_name,_value));
     }
-    else if (tmp == 'I')
+    else if (tmp == 'I')            //Current source added to _circuit
     {
         double _current;
         src >> _name >> _anode >> _cathode >> _current;
         _circuit.add_component(Current_source(_anode, _cathode, _name, _current));
-
+    }
+    else if (tmp == 'D')            //Diode added to _circuit
+    {   
+        src >> _name >> _anode >> _cathode;
+        _circuit.add_component(Diode(_anode,_cathode,_name));
     }
 
     src.close();
