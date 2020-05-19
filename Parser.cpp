734bcc91bf5b4540b1149eef0789a156bc824925
lg519 <lg519@ic.ac.uk>
2020-05-19 22:59:52 +0100

    transformed class into function

--- Parser.cpp
+++ Parser.cpp
@@ -4,44 +4,45 @@
 #include<cctype>
 #include<cmath>
 
-class Power_of_Ten
+
+//declaring constants
+double femto = pow(10,-15);
+double pico = pow(10,-12);
+double nano = pow(10,-9);
+double micro = pow(10,-6);
+double milli = pow(10,-3);
+double kilo = pow(10,3);
+double mega = pow(10,6);
+double giga = pow(10,9);
+double tera = pow(10,12);
+
+double mil = 25.4*pow(10,-6);
+
+/*
+enables reading of prefixes such as kilo, mega ...
+*/
+double read_power_of_ten(std::istream& src)
 {
-private:
-    double femto = pow(10,-15);
-    double pico = pow(10,-12);
-    double nano = pow(10,-9);
-    double micro = pow(10,-6);
-    double milli = pow(10,-3);
-    double kilo = pow(10,3);
-    double mega = pow(10,6);
-    double giga = pow(10,9);
-    double tera = pow(10,12);
+    std::string tmp;
+    if(src >> tmp)
+    {            
+        char c = tolower(tmp[0]);
 
-    double mil = 25.4*pow(10,-6);
-
-public:
-    double read_power_of_ten(std::istream& src) const
-    {
-        std::string tmp;
-        if(src >> tmp)
-        {            
-            char c = tolower(tmp[0]);
-
-            if(c == 'f'){return femto;}
-            else if(c == 'p'){return pico;}
-            else if(c == 'n'){return nano;}
-            else if(c == 'u'){return micro;}
-            else if(c == 'm'){return milli;}
-            else if(c == 'k'){return kilo;}
-            else if(c == 'm' && tolower(tmp[1]) == 'e' && tolower(tmp[2]) == 'g'){return mega;}
-            else if(c == 'g'){return giga;}
-            else if(c == 't'){return tera;}
-            else if(c == 'm' && tolower(tmp[1]) == 'i' && tolower(tmp[2]) == 'l'){return mil;}
-        }
-        return 1; 
+        if(c == 'f'){return femto;}
+        else if(c == 'p'){return pico;}
+        else if(c == 'n'){return nano;}
+        else if(c == 'u'){return micro;}
+        else if(c == 'm'){return milli;}
+        else if(c == 'k'){return kilo;}
+        else if(c == 'm' && tolower(tmp[1]) == 'e' && tolower(tmp[2]) == 'g'){return mega;}
+        else if(c == 'g'){return giga;}
+        else if(c == 't'){return tera;}
+        else if(c == 'm' && tolower(tmp[1]) == 'i' && tolower(tmp[2]) == 'l'){return mil;}
     }
+    return 1; 
+}
 
-};
+
 
 
 
@@ -80,18 +81,20 @@ int main(int argc, char const *argv[])
         if (tmp == 'R' | tmp == 'r')             //Resistor added to _circuit
         {    
             src >> _name >> _anode >> _cathode >> _value;
-            _value *= 
+            _value *= read_power_of_ten(src);
             _circuit.add_component(Resistor(_anode,_cathode,_name,_value));
         }
         else if (tmp == 'L' | tmp == 'l')            //Inductor added to _circuit
         {
             src >> _name >> _anode >> _cathode >> _value;
+            _value *= read_power_of_ten(src);
             _circuit.add_component(Inductor(_anode,_cathode,_name,_value));
         }
         else if (tmp == 'I')            //Current source added to _circuit
         {
             double _current;
             src >> _name >> _anode >> _cathode >> _current;
+            _current *= read_power_of_ten(src);
             _circuit.add_component(Current_source(_anode, _cathode, _name, _current));
         }
         else if (tmp == 'D')            //Diode added to _circuit
@@ -103,11 +106,13 @@ int main(int argc, char const *argv[])
         {
             double _voltage;
             src >> _name >> _anode >> _cathode >> _voltage;
+            _voltage *= read_power_of_ten(src);
             _circuit.add_component(Voltage_Source(_anode, _cathode, _name, _voltage));
         }
         else if (tmp == 'C' | tmp == 'c')           //Capacitor added to _circuit
         {
             src >> _name >> _anode >> _cathode >> _value;
+            _value *= read_power_of_ten(src);
             _circuit.add_component(Capacitor(_anode,_cathode,_name,_value));
         }
         else if (tmp == 'E')            //Voltage_Controlled_Voltage_Source added to _circuit
@@ -116,6 +121,7 @@ int main(int argc, char const *argv[])
             int _control_voltage_anode;
             int _control_voltage_cathode;
             src >> _name >> _anode >> _cathode >> _control_voltage_anode >> _control_voltage_cathode >> _gain;
+            _gain *= read_power_of_ten(src);
             _circuit.add_component(Voltage_Controlled_Voltage_Source(_anode, _cathode, _name, _gain, _control_voltage_anode, _control_voltage_cathode));
 
         }
@@ -125,6 +131,7 @@ int main(int argc, char const *argv[])
             int _control_voltage_anode;
             int _control_voltage_cathode;
             src >> _name >> _anode >> _cathode >> _control_voltage_anode >> _control_voltage_cathode >> _gain;
+            _gain *= read_power_of_ten(src);
             _circuit.add_component(Voltage_Controlled_Current_Source(_anode, _cathode, _name, _gain, _control_voltage_anode, _control_voltage_cathode));
         }
         else
