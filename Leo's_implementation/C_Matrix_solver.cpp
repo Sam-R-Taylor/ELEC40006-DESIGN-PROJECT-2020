20951f194d952c50e941ffd489f303e1a0c4bea2
lg519 <lg519@ic.ac.uk>
2020-05-21 10:49:09 +0100

    some changes

---
+++ Leo's_implementation/C_Matrix_solver.cpp
@@ -0,0 +1,13 @@
+#include"Parser.hpp"
+#include"Leo_Component.hpp"
+
+
+
+
+
+
+
+int main(int argc, char const *argv[])
+{   
+    Circuit input_circuit = Parse_input(argv[1]);
+}
---
+++ Leo's_implementation/Leo_Component.hpp
@@ -0,0 +1,81 @@
+#ifndef LEO_COMPONENT_HPP
+#define LEO_COMPONENT_HPP
+
+#include<iostream>
+#include<string>
+#include<cmath>
+#include<vector>
+
+
+
+
+class Component
+{
+protected:
+    std::string name;
+    int anode;
+    int cathode;
+public:                          
+    virtual ~Component(){}
+    /*
+    reurns the anode of the component
+    */     
+    int get_anode() const {return anode;}
+    /*
+    reurns the cathode of the component
+    */  
+    int get_cathode() const {return cathode;}
+    /*
+    reurns the name of the component
+    */  
+    std::string get_name() const {return name;}
+};
+
+
+
+
+class Resistor :
+    public Component
+{
+private:
+    double resistance;
+public:
+    Resistor(int _anode, int _cathode, std:: string _name, double _resistance){
+        anode = _anode;
+        cathode = _cathode;
+        name = _name;
+        resistance = _resistance;
+    }
+    double get_resistance() const{
+        return resistance;
+    }
+    double get_current(const std::vector<double> &nodevoltages) const
+    {
+        double current = (nodevoltages[anode]-nodevoltages[cathode])/resistance;
+        return current;
+    }
+};
+
+
+
+
+class Current_source :
+    public Component
+{
+private:
+    /*
+    fixed current going from anode to cathode
+    */
+    double current;
+public:
+    Current_source(int _anode, int _cathode, std:: string _name, double _current){
+        anode = _anode;
+        cathode = _cathode;
+        name = _name;
+        current = _current;
+    }
+    double get_current() const {return current;}
+};    
+
+
+#endif
