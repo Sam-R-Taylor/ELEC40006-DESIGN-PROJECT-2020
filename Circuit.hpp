59c23b8b5afa9d38fa45edcc0e0a4b139e31a8ca
lg519 <lg519@ic.ac.uk>
2020-05-22 16:21:02 +0100

    Easier to read Parser Better Circuit.hpp

--- Circuit.hpp
+++ Circuit.hpp
@@ -20,6 +20,34 @@ struct Node{
         }
         */
 };
+
+
+//helper function for creating Circuit::std::vector<Node> nodes;
+std::vector<Node> NodeGenerator(std::vector<Component> components){
+    std::vector<Node> Nodes;
+    //iterate through the components
+    int index = 0;
+    for(Component component: components){ 
+        //ensure the node list has a node of high enough index 
+        while(Nodes.size()<=component.get_cathode()){
+            Node node;
+            node.index = index;
+            Nodes.push_back(node);
+            index++;
+        }
+        while(Nodes.size()<=component.get_anode()){
+            Node node;
+            node.index = index;
+            Nodes.push_back(node);
+            index++;
+        }
+        //add the components to the nodes it is attached to
+        Nodes[component.get_cathode()].components_attached.push_back(component);
+        Nodes[component.get_anode()].components_attached.push_back(component);
+    }
+    return Nodes;
+}
+
 class Circuit
 {
 protected:
@@ -27,32 +55,21 @@ protected:
     std::vector<Component> components;
     std::vector<double> voltages;
 public:
-    void set_voltages(std::vector<double> _voltages){
-        voltages = _voltages;
-    }
     std::vector<double> get_voltages() const{
         return voltages;
-    }
-    void add_component(const Component& _component)
-    {
-        components.push_back(_component);
-    }
-    void add_node(const Node& _node)
-    {
-        nodes.push_back(_node);
-    }
-    Node get_node(int index) const{
-        return nodes[index];
-    }
-    int get_number_of_nodes() const{
-        return nodes.size();
-    }
+    }    
     std::vector<Node> get_nodes() const{
         return nodes;
     }
     std::vector<Component> get_components() const{
         return components;
     }
+    int get_number_of_nodes() const{
+        return nodes.size();
+    }
+    void build_components(const std::vector<Component> _components){components = _components;}
+    void build_nodes(const std::vector<Node> _nodes){nodes = _nodes;}
+    void set_voltages(std::vector<double> _voltages){voltages = _voltages;}
 };
 
 
