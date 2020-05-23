b21081c71294ac053d694b344bff92082cc30b77
lg519 <lg519@ic.ac.uk>
2020-05-23 11:38:24 +0100

    added helper functions

--- leo_KCLSolver.cpp
+++ leo_KCLSolver.cpp
@@ -4,18 +4,28 @@
 #include<Eigen/Dense>
 
 
+//helper functions for Matrix_solver
+void remove_Row(Eigen::MatrixXd& matrix, unsigned int rowToRemove)
+{
+    unsigned int numRows = matrix.rows()-1;
+    unsigned int numCols = matrix.cols();
 
+    if( rowToRemove < numRows )
+        matrix.block(rowToRemove,0,numRows-rowToRemove,numCols) = matrix.block(rowToRemove+1,0,numRows-rowToRemove,numCols);
 
+    matrix.conservativeResize(numRows,numCols);
+}
 
+void remove_Column(Eigen::MatrixXd& matrix, unsigned int colToRemove)
+{
+    unsigned int numRows = matrix.rows();
+    unsigned int numCols = matrix.cols()-1;
 
+    if( colToRemove < numCols )
+        matrix.block(0,colToRemove,numRows,numCols-colToRemove) = matrix.block(0,colToRemove+1,numRows,numCols-colToRemove);
 
-
-
-
-
-
-
-
+    matrix.conservativeResize(numRows,numCols);
+}
 
 
 
