9e605c3be6342c6b967f4e684d6d463b1c5e5af9
Sam-R-Taylor <samrtayloradm@gmail.com>
2020-05-23 14:26:27 +0100

    Added File Read Write functionality to UI

--- SLMCircuitSim/mainwindow.cpp
+++ SLMCircuitSim/mainwindow.cpp
@@ -1,5 +1,8 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
+#include <QMessageBox>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -13,3 +16,32 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QFile file("Output.txt");
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)){
+        QMessageBox::warning(this,"Title","File Not Open");
+
+    }
+    QTextStream out(&file);
+    QString text = ui->plainTextEdit_2->toPlainText() ;
+    out << text ;
+    file.flush(); //Flushes stream inside out
+    file.close();
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QFile file("Input.txt");
+
+    if (!file.open(QFile::ReadOnly | QFile::Text)){
+        QMessageBox::warning(this,"Title","File Not Open");
+
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->plainTextEdit_2->setPlainText(text);
+    file.close();
+}
--- SLMCircuitSim/mainwindow.h
+++ SLMCircuitSim/mainwindow.h
@@ -2,6 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -14,6 +15,11 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_pushButton_clicked();
+
+    void on_pushButton_2_clicked();
 
 private:
     Ui::MainWindow *ui;
