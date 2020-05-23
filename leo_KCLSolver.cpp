a91353314cb3b8987e36c90663649d26fcd67dc8
lg519 <lg519@ic.ac.uk>
2020-05-23 11:35:19 +0100

    leo_KCL added current_source

--- leo_KCLSolver.cpp
+++ leo_KCLSolver.cpp
@@ -24,20 +24,27 @@
 
 
 
-Eigen::MatrixXd return_Conductance_Matrix(const Circuit& input_circuit)
+Eigen::VectorXd Matrix_solver(const Circuit& input_circuit)
 {   
     //initializing a matrix of the right size 
     int Mat_size = input_circuit.get_number_of_nodes();     
     Eigen::MatrixXd Mat(Mat_size,Mat_size);
 
+    //initializing a vector of the right size
+    Eigen::VectorXd Vec(Mat_size);
+
     //Fills up the matrix with 0s
     Mat.Zero(Mat_size,Mat_size);
+
+    //Fills up the vector with 0s
+    Vec.Zero();
 
 
     //nodes needed to set up the KCL equations
     std::vector<Node> nodes = input_circuit.get_nodes();
 
     //initializing each element of the conductance matrix
+    //sum of currents out of a node = 0
     for (int row = 0; row < Mat_size; row++)
     {
         int node_index = row;
@@ -62,6 +69,21 @@ Eigen::MatrixXd return_Conductance_Matrix(const Circuit& input_circuit)
                     Mat(node_index,cathode)+= Rptr->get_conductance();
                     Mat(node_index,anode)-= Rptr->get_conductance();
                 }
+            }
+            if(dynamic_cast<Current_source*>(component_attached))
+            {
+                int anode = component_attached->get_anode();
+                int cathode = component_attached->get_cathode();
+                Current_source* Currentptr = dynamic_cast<Current_source*>(component_attached);
+
+                if(anode == node_index)
+                {
+                    Vec(node_index) += Currentptr->get_current();
+                }
+                else if (cathode == node_index)
+                {
+                    Vec(node_index) -= Currentptr->get_current();
+                }
 
             }
         }
@@ -78,12 +100,6 @@ Eigen::MatrixXd return_Conductance_Matrix(const Circuit& input_circuit)
 
 
 
-/*
-given a Circuit 
-
-returns a Eigen::VectorXd of voltages (i.e. the solution to the circuit)
-*/
-Eigen::VectorXd Matrix_solver(const Circuit& input_circuit){}
 
 
 
