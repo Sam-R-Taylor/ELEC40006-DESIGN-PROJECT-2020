92f238aed523ba78071317585958a2136721dca8
lg519 <lg519@ic.ac.uk>
2020-05-23 08:34:14 +0100

    Rmoved Node struct

--- Circuit.hpp
+++ Circuit.hpp
@@ -6,53 +6,42 @@
 #include "Component.hpp"
 
 
-struct Node{
-        int index = 0;
-        std::vector<Component> components_attached;
-        /*
-        void set_index(int _index){index = _index;}
-        int get_index(){return index;}
-        void add_component(Component* component){
-            components.push_back(component);
-        }
-        vector<Component*> get_components(){
-            return components;
-        }
-        */
-};
 class Circuit
 {
 protected:
-    std::vector<Node> nodes;
+    std::vector<std::vector<Component*>> nodes;
     std::vector<Component> components;
     std::vector<double> voltages;
 public:
     void set_voltages(std::vector<double> _voltages){
         voltages = _voltages;
     }
-    std::vector<double> get_voltages() const{
-        return voltages;
-    }
     void add_component(const Component& _component)
     {
         components.push_back(_component);
     }
-    void add_node(const Node& _node)
+    void add_node(Component& _node_component)
     {
-        nodes.push_back(_node);
-    }
-    Node get_node(int index) const{
-        return nodes[index];
+        int anode = _node_component.get_anode();
+        int cathode = _node_component.get_cathode();
+        if (anode > nodes.size())
+        {
+            nodes.resize(anode);
+        }
+        else if (cathode > nodes.size())
+        {
+            nodes.resize(cathode);
+        }
+        nodes[anode].push_back(&_node_component);
+        nodes[cathode].push_back(&_node_component);
+        
     }
     int get_number_of_nodes() const{
         return nodes.size();
     }
-    std::vector<Node> get_nodes() const{
-        return nodes;
-    }
-    std::vector<Component> get_components() const{
-        return components;
-    }
+    std::vector<std::vector<Component*>> get_nodes() const{return nodes;}
+    std::vector<Component> get_components() const{return components;}
+    std::vector<double> get_voltages() const{return voltages;}
 };
 
 
