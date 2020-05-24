c00bd2bbd9856358ff943b63d0b4544e5322aebd
lg519 <lg519@ic.ac.uk>
2020-05-24 09:43:27 +0100

    circuit.hpp has vector<Componenet*>

--- Circuit.hpp
+++ Circuit.hpp
@@ -14,27 +14,27 @@ struct Node{
 
 
 
-std::vector<Node> NodeGenerator(std::vector<Component> components){
+std::vector<Node> NodeGenerator(std::vector<Component*> components){
     std::vector<Node> Nodes;
     //iterate through the components
     int index = 0;
     for(int i=0; i < components.size(); i++){ 
         //ensure the node list has a node of high enough index 
-        while(Nodes.size()<=components[i].get_cathode()){
+        while(Nodes.size()<=components[i]->get_cathode()){
             Node node;
             node.index = index;
             Nodes.push_back(node);
             index++;
         }
-        while(Nodes.size()<=components[i].get_anode()){
+        while(Nodes.size()<=components[i]->get_anode()){
             Node node;
             node.index = index;
             Nodes.push_back(node);
             index++;
         }
         //add the components to the nodes it is attached to
-        Nodes[components[i].get_cathode()].components_attached.push_back(&components[i]);
-        Nodes[components[i].get_anode()].components_attached.push_back(&components[i]);
+        Nodes[components[i]->get_cathode()].components_attached.push_back(components[i]);
+        Nodes[components[i]->get_anode()].components_attached.push_back(components[i]);
     }
     return Nodes;
 }
@@ -46,20 +46,20 @@ class Circuit
 {
 protected:
     std::vector<Node> nodes;
-    std::vector<Component> components;
+    std::vector<Component*> components;
     std::vector<double> voltages;
 public:
     void set_voltages(std::vector<double> _voltages){
         voltages = _voltages;
     }
-    void add_component(const Component& _component)
+    void add_component(Component* _component)
     {
         components.push_back(_component);
     }
     void print_components() const
     {
-        for(Component i: components){
-            std::cerr << i.get_name()<< std::endl;
+        for(Component* i: components){
+            std::cerr << i->get_name()<< std::endl;
         }
 
     }
@@ -85,7 +85,7 @@ public:
         return nodes.size();
     }
     std::vector<Node> get_nodes() const{return nodes;}
-    std::vector<Component> get_components() const{return components;}
+    std::vector<Component*> get_components() const{return components;}
     std::vector<double> get_voltages() const{return voltages;}
 };
 
