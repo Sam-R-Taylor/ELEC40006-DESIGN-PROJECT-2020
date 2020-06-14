e682e80e087fc29b96368b1ef3c9f6acb8e98a58
max-wickham <mw1919@ic.ac.uk>
2020-06-14 14:03:58 +0100

    complete

--- test_main.cpp
+++ test_main.cpp
@@ -1,9 +1,11 @@
-
+/*
 #include"Parser.hpp"
 #include<fstream>
 int main(int argc, char const *argv[])
 {
-    std::fstream input(argv[1]);
-    parse_input(input);
+    for(int i = 0; i < 30000; i++){
+        std::fstream input(argv[1]);
+        parse_input(input);
+    }
     return 0;
-}
+}*/
